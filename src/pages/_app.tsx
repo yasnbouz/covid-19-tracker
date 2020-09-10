df763ef84dea6aea4d34bee3cf327de470d1cd80
yasnbouzi <elbouziady.yassine@gmail.com>
2020-09-10 21:19:21 +0100

    Add G analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
+
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import AppLayout from 'components/layouts/AppLayout';
+import * as gtag from 'lib/gtag';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
     return (
         <AppLayout>
             <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
+import { GA_TRACKING_ID } from 'lib/gtag';
+
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
@@ -9,7 +11,22 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${GA_TRACKING_ID}', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
