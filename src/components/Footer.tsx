065b3328c545a092ec110dff7fad68136f838d21
yasnbouzi <elbouziady.yassine@gmail.com>
2021-02-25 16:30:47 +0100

    update github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,8 +13,8 @@ export default function Footer() {
         >
             <p>
                 Made with <span sx={{ color: 'warning' }}>❤</span> by{' '}
-                <Link href="https://github.com/yasnbouzi" target="_blank" rel="noopener" style={{ color: '#055dbd' }}>
-                    @yasnbouzi
+                <Link href="https://github.com/yasnbouz" target="_blank" rel="noopener" style={{ color: '#055dbd' }}>
+                    @yasnbouz
                 </Link>
             </p>
         </footer>
